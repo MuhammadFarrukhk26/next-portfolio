9829358ba7ec5df7c0fda430640c11e2271d936c
MuhammadFarrukhk26 <muhammadfarrukhk26@gmail.com>
2024-09-18 01:56:59 +0500

    work page initiated

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,22 @@
-const Work = () => {
-  return (
-    <div>Work Page</div>
-  )
-}
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+// import {Swiper, SwiperSlide} from "swiper/react"
+// import "swiper/css"
+import { BsArrowwUpRight, BsGithub } from "react-icons/bs";
 
-export default Work
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+const Work = () => {
+  return <div>Work Page</div>;
+};
+
+export default Work;
