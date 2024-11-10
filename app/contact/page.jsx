0f7e71f732172f3957e7d68a9723d8602e6ab57b
MuhammadFarrukhk26 <muhammadfarrukhk26@gmail.com>
2024-11-10 20:43:06 +0500

    changes

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+92) 337 801 9687",
+  },
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "muhammadfarrukhk26@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt />,
+    title: "Address",
+    description: "Gulshan-e-Iqbal, Karachi",
+  },
+];
+
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div>Contact page</div>
-  )
-}
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form>form</form>
+          </div>
+          {/* Info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:oreder-none mb-8 xl:mb-0">
+            info
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
 
-export default Contact
+export default Contact;
